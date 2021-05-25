1990f39222274ecfa0b779d0c80821cfb8f2381b
ginstone <hjg719@139.com>
2021-05-25 10:21:13 +0800

    SN码验证

--- src/components/management/goods/form/inventory-in-order-form.vue
+++ src/components/management/goods/form/inventory-in-order-form.vue
@@ -1,29 +1,29 @@
 <template>
- <el-table :data="list">
-   <el-table-column label="型号" width="60px" prop="model"/>
-   <el-table-column label="尺寸" width="50px" prop="size"/>
-   <el-table-column label="颜色" width="50px" prop="color"/>
-   <el-table-column label="SN"  prop="sn"/>
-   <el-table-column label="SN">
-     <template slot-scope="s">
-<!--       <el-input v-model = "s.row.sn" @focus="$event.currentTarget.select()" @change="updateSn(s.row.uuid,s.row.sn)" />-->
-       <el-upload
-         :data="{uuid:s.row.uuid}"
-         action="/api/InventoryInOrder/uploadSn"
-         :on-success="findByOrderUuid"
-         >
-         <el-button type="primary" size="small" ><i class="el-icon-plus" /></el-button>
-       </el-upload>
-     </template>
-   </el-table-column>
+  <el-table :data="list">
+    <el-table-column label="型号" prop="model" width="60px"/>
+    <el-table-column label="尺寸" prop="size" width="50px"/>
+    <el-table-column label="颜色" prop="color" width="50px"/>
+    <el-table-column label="SN" prop="sn"/>
+    <el-table-column label="SN">
+      <template slot-scope="s">
+        <!--       <el-input v-model = "s.row.sn" @focus="$event.currentTarget.select()" @change="updateSn(s.row.uuid,s.row.sn)" />-->
+        <el-upload
+          :data="{uuid:s.row.uuid}"
+          :on-success="findByOrderUuid"
+          action="/api/InventoryInOrder/uploadSn"
+        >
+          <el-button size="small" type="primary"><i class="el-icon-plus"/></el-button>
+        </el-upload>
+      </template>
+    </el-table-column>
 
 
- </el-table>
+  </el-table>
 
 </template>
 
 <script>
-import {findByOrderUuid, updateSn} from "../../../../assets/js/api/order/order";
+import {findByOrderUuid} from "../../../../assets/js/api/order/order";
 import {copyObj} from "../../../../assets/js/utils";
 import {baseFindAll} from "../../../../assets/js/api/baseApi";
 
@@ -32,30 +32,34 @@ export default {
   data() {
     return {
       myData: {},
-      list:[],
-      inventory:[],
+      list: [],
+      inventory: [],
     }
   },
   methods: {
     copy(obj) {
       this.myData = obj ? copyObj(obj) : [];
     },
-    updateSn(uuid,sn) {
-      updateSn(uuid,sn,(res) => this.$message(res.message)).catch((res) => this.$message(res.message))
-    },
-    findByOrderUuid(){
-      findByOrderUuid(this.myData).then(res=>{
-        this.list = res.data.map(item=>{
-          let element = this.inventory.filter(i=>i.uuid===item.inventoryUuid)[0];
-          return Object.assign({},element,item);
+    findByOrderUuid(response) {
+      if (!response) {
+        return;
+      }
+      if (response.code === 2000) {
+        findByOrderUuid(this.myData).then(res => {
+          this.list = res.data.map(item => {
+            let element = this.inventory.filter(i => i.uuid === item.inventoryUuid)[0];
+            return Object.assign({}, element, item);
+          })
+          console.log(this.list)
         })
-        console.log(this.list)
-      })
+      } else {
+        this.$message(response.message)
+      }
     },
     findAllInventory() {
       baseFindAll("/Inventory").then(res => {
         this.inventory = res.data;
-        this.findByOrderUuid();
+        this.findByOrderUuid({code: 2000});
       })
     },
   },
