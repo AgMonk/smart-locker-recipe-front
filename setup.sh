58ace76b4430f72c89fa2651338aec8950a2277b
ginstone <hjg719@139.com>
2021-05-29 14:23:47 +0800

    Jenkins 自动构建

--- setup.sh
+++ setup.sh
@@ -10,4 +10,4 @@ docker run -p 18900:80 -d --name smart-locker-recipe-front smart-locker-recipe-f
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
 docker logs smart-locker-recipe-front;
 #删除build过程中产生的镜像    #docker image prune -a -f
-docker rmi $(docker images -f "dangling=true" -q)
+#docker rmi $(docker images -f "dangling=true" -q)
